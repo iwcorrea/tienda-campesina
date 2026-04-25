78ff890fc3d072950a8b38d4178cbd8f524deb8f
iwcorrea <133600521+iwcorrea@users.noreply.github.com>
2026-04-25 13:11:04 -0500

    cambios finales para correr

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -3,3 +3,6 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
+
+# Forzar el uso de un diccionario vacío para la caché (evita el error de clave no hasheable)
+templates.env.cache = {}
